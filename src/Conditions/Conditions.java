dd3d340546c207d88226a616fa4cb1a7d9a2ff38
Valen <valentinhamidullin78@gmail.com>
2023-01-16 18:16:01 +0300

    Conditions

---
+++ src/Conditions/Conditions.java
@@ -0,0 +1,21 @@
+package Conditions;
+
+
+
+public class Conditions {
+    public static void main(String[] args) {
+
+        int count = 250;
+        if (count < 100) {
+            System.out.println("Мало");
+        } else if (count < 200) {
+            System.out.println("Средне");
+        }
+         else {
+            System.out.println("Много");
+        }
+
+
+
+    }
+}
