16ad87fd1dd6a2e78afa4092ecd570a45134e325
Valen <valentinhamidullin78@gmail.com>
2023-01-16 20:13:18 +0300

    Topography13

--- src/Projects/Topography.java
+++ src/Projects/Topography.java
@@ -1,51 +1,42 @@
 package Projects;
 
 public class Topography {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         int paperReserve = 600000;
         int inkReserve = 600000;
         int coverReserve = 2000;
-
         double printRollerMinTemp = 80.5;
         double printRollerMaxTemp = 120.75;
-
         int pagesInBook = 530;
         int booksCount = 2001;
-
         boolean coldPrintingMode = false;
         double printRollerTemp = 90;
-
         int papersCount = booksCount * pagesInBook / 2;
         int inkedPapersCount = papersCount + booksCount;
-
-        boolean printRollerTempForPrinting = printRollerMinTemp <= printRollerTemp && printRollerTemp <= printRollerMaxTemp;
         boolean paperIsEnough = papersCount <= paperReserve;
         boolean inkIsEnough = inkedPapersCount <= inkReserve;
         boolean coversAreEnough = booksCount <= coverReserve;
-
+        boolean rollerTempIsNormal = printRollerTemp >= printRollerMinTemp && printRollerTemp <= printRollerMaxTemp;
         boolean errors = false;
-
         if (!paperIsEnough || !inkIsEnough) {
             errors = true;
             System.out.println("Бумаги или чернил недостаточно");
         } else {
             if (coversAreEnough) {
-                if (!coldPrintingMode && !printRollerTempForPrinting) {
+                if (!coldPrintingMode && !rollerTempIsNormal) {
                     errors = true;
-                    System.out.println("Неверный режим печати или температуры печатающего ролла недопустимая");
+                    System.out.println("Неверный режим печати или температура печатающего ролла недопустимая");
+                }
             } else {
                 errors = true;
-                System.out.println("Обложек недостаточно");
-                }
-            }
+            System.out.println("Обложек недостаточно");
         }
-
-        if (!errors) {
-            System.out.println("Печать разрешена");
-        } else {
-            System.out.println("Печать запрещена");
-        }
-
     }
+if (!errors) {
+        System.out.println("Печать разрешена");
+    } else {
+        System.out.println("Печать запрещена");
+    }
+}
 }
