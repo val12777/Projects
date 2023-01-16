9756ef265ecb49e30de53c2aaf8772063103467e
Valen <valentinhamidullin78@gmail.com>
2023-01-16 18:33:27 +0300

    Conditions

---
+++ src/Projects/InternetStore.java
@@ -0,0 +1,17 @@
+package Projects;
+
+public class InternetStore {
+    public static void main(String[] args) {
+        int maxWeight = 5000;
+        int minCost = 1000;
+
+        int weight = 6000;
+        int cost = 1750;
+
+        if (weight < maxWeight && cost >= minCost) {
+
+            System.out.println("Доставка бесплатная");
+
+        }
+    }
+}
